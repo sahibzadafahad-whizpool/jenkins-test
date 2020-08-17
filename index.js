5839ae8bba48d4396f9c219b52a24e6bd13c61fd
sahibzadafahad-whizpool <sahibzadafahad-whizpool>
2020-08-17 10:26:17 +0000

    updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello Node!')
+  res.send('Hello Node, Hi from Jenkins!')
 })
 
 app.listen(8080, function () {
