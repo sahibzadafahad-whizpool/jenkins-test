f3969937f6262fdc634ffe38e24e6c70ba054da1
sahibzadafahad-whizpool <sahibzada.fahad@whizpool.com>
2020-08-17 15:02:11 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
   res.send('Hello Node!')
 })
 
-app.listen(3000, function () {
-  console.log('Listening on port 3000...')
+app.listen(8080, function () {
+  console.log('Listening on port 8080...')
 })
