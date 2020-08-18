9f0c0522e4c0c1d4217d3e7171d386fd4839a125
sahibzadafahad-whizpool <sahibzadafahad-whizpool>
2020-08-18 05:44:21 +0000

    updated

--- client/src/app/config.ts
+++ client/src/app/config.ts
@@ -1,4 +1,6 @@
 // dev / local
 
-export const SERVER = 'https://schoolapp.doctors-finder.com/api/';
-export const IMAGE_BASE = 'https://schoolapp.doctors-finder.com/';
+#export const SERVER = 'https://schoolapp.doctors-finder.com/api/';
+#export const IMAGE_BASE = 'https://schoolapp.doctors-finder.com/';
+export const SERVER = 'http://3.84.238.66/api/';
+export const IMAGE_BASE = 'http://3.84.238.66/';
